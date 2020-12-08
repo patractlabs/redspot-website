bb804edcf3b1218c09ecec3227cea3c3b19c0e48
ii-ii-ii <69485494+ii-ii-ii@users.noreply.github.com>
2020-12-08 17:14:10 +1300

    Navbar style for mobile

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,7 +1,17 @@
-const navBurger = document.querySelector('.navbar-burger');
-const navMenu = document.querySelector('.navbar-menu');
+const navBurger = document.querySelector(".navbar-burger");
+const navMenu = document.querySelector(".navbar-menu");
 
-navBurger.addEventListener('click', () => {
-    navBurger.classList.toggle('is-active');
-    navMenu.classList.toggle('is-active');
+navBurger.addEventListener("click", (event) => {
+  event.stopPropagation();
+  navBurger.classList.toggle("is-active");
+  navMenu.classList.toggle("is-active");
+});
+
+navMenu.addEventListener("click", (event) => {
+  event.stopPropagation();
+});
+
+window.addEventListener("click", () => {
+  navBurger.classList.remove("is-active");
+  navMenu.classList.remove("is-active");
 });
