7961e7611f3196fd3950268512f905e7e5e03410
ii-ii-ii <69485494+ii-ii-ii@users.noreply.github.com>
2020-12-09 21:54:04 +1300

    Inline css and js files

--- .eleventy.js
+++ .eleventy.js
@@ -1,47 +1,46 @@
 const { DateTime } = require("luxon");
-const fs = require("fs");
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const pluginTOC = require("eleventy-plugin-nesting-toc");
-const i18n = require('eleventy-plugin-i18n');
+const i18n = require("eleventy-plugin-i18n");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
-const markdownItAttrs = require('markdown-it-attrs');
-const translations = require('./src/_data/i18n');
+const markdownItAttrs = require("markdown-it-attrs");
+const translations = require("./src/_data/i18n");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
   eleventyConfig.addPlugin(pluginNavigation);
-  eleventyConfig.addPlugin(
-		pluginTOC,
-		{
-			tags: ["h1", "h2", "h3", "h4"],
-			wrapper: "div  ",
-      wrapperClass: "toc",
-      headingText: '',
-		},
-  );
+  eleventyConfig.addPlugin(pluginTOC, {
+    tags: ["h1", "h2", "h3", "h4"],
+    wrapper: "div  ",
+    wrapperClass: "toc",
+    headingText: "",
+  });
   eleventyConfig.addPlugin(i18n, {
     translations,
     fallbackLocales: {
-      '*': 'en'
-    }
+      "*": "en",
+    },
   });
+  // eleventyConfig.setLibrary("njk", nunjucksEnvironment);
 
   eleventyConfig.setDataDeepMerge(true);
 
-  eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
+      "dd LLL yyyy"
+    );
   });
 
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
   });
 
   // Get the first `n` elements of a collection.
   eleventyConfig.addFilter("head", (array, n) => {
-    if( n < 0 ) {
+    if (n < 0) {
       return array.slice(n);
     }
 
@@ -58,31 +57,28 @@ module.exports = function(eleventyConfig) {
   let markdownLibrary = markdownIt({
     html: true,
     breaks: true,
-    linkify: true
-  }).use(markdownItAnchor, {
-    permalink: true,
-    permalinkSymbol: "",
-    permalinkAttrs: (slug) => ({"aria-label": slug}),
-    slugify: (title) => {
-      return encodeURIComponent(
-        String(title).trim().toLowerCase().replace(/\s+/g, "-"),
-      );
-    },
-  }).use(markdownItAttrs, {
-    // optional, these are default options
-    leftDelimiter: '{',
-    rightDelimiter: '}',
-    allowedAttributes: []  // empty array = all attributes are allowed
-  });
+    linkify: true,
+  })
+    .use(markdownItAnchor, {
+      permalink: true,
+      permalinkSymbol: "",
+      permalinkAttrs: (slug) => ({ "aria-label": slug }),
+      slugify: (title) => {
+        return encodeURIComponent(
+          String(title).trim().toLowerCase().replace(/\s+/g, "-")
+        );
+      },
+    })
+    .use(markdownItAttrs, {
+      // optional, these are default options
+      leftDelimiter: "{",
+      rightDelimiter: "}",
+      allowedAttributes: [], // empty array = all attributes are allowed
+    });
   eleventyConfig.setLibrary("md", markdownLibrary);
 
   return {
-    templateFormats: [
-      "md",
-      "njk",
-      "html",
-      "liquid"
-    ],
+    templateFormats: ["md", "njk", "html", "liquid"],
 
     // If your site lives in a different subdirectory, change this.
     // Leading or trailing slashes are all normalized away, so don’t worry about those.
@@ -103,7 +99,7 @@ module.exports = function(eleventyConfig) {
       input: "src",
       includes: "_includes",
       data: "_data",
-      output: "_site"
-    }
+      output: "_site",
+    },
   };
 };
